ad0f04a59227a2439098a5b18ba6394fae79357c
Guilherme Terenciani <guilhermeterenciani@gmail.com>
2022-06-21 17:25:36 -0300

    Exemplo para o Adriano

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function abreMenu() {
+    let menu = document.querySelector(".menu");
+    if(menu.style.visibility=="visible"){
+        menu.style.visibility="hidden"; 
+    }
+    else{
+        menu.style.visibility="visible";
+    }
+  
+  //background-img="https://avatars.githubusercontent.com/u/8595275?v=4";
+}
